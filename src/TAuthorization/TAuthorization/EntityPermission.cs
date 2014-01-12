ea41abd65c079302fb0a6c0f56f7f4e483451668
vahid <cevtorabi@gmail.com>
2014-01-12 19:42:26 +0330

    Set entity id to nullable in entity permission

--- src/TAuthorization/TAuthorization/EntityPermission.cs
+++ src/TAuthorization/TAuthorization/EntityPermission.cs
@@ -8,7 +8,7 @@ namespace TAuthorization
         private Dictionary<string, string> _rawActionParams = new Dictionary<string, string>();
 
         public virtual Guid Id { get; set; }
-        public virtual Guid EntityId { get; set; }
+        public virtual Guid? EntityId { get; set; }
         public virtual string ActionName { get; set; }
         public virtual string ActionCategory { get; set; }
         public virtual string RoleName { get; set; }
