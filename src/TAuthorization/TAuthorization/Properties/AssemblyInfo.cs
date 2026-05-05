caa7f01ee1db7528493331f7fffbc7fcfc6d3611
h.torkemandi <h.torkemandi@H-TORKEMANDI>
2026-05-05 15:27:08 +0800

    Copyright notice changed

--- src/TAuthorization/TAuthorization/Properties/AssemblyInfo.cs
+++ src/TAuthorization/TAuthorization/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TAuthorization")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ISC CONSOLSYS Philippines 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
